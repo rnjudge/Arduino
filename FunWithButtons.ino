9130873bf1320174fcf070981d5c7b8928739363
Rose Judge <rose.harber@gmail.com>
2018-03-08 14:06:10 -0800

    Clean it up

--- FunWithButtons.ino
+++ FunWithButtons.ino
@@ -1,39 +1,28 @@
 //www.elegoo.com
 //2016.12.08
 
-int ledPin = 5;
+int blueLED = 5;
 int greenLED = 6;
 int buttonApin = 9;
 int buttonBpin = 8;
 
-byte leds = 0;
-
+int blueVal = 0;
+int greenVal = 0;
 void setup() 
 {
   pinMode(greenLED, OUTPUT);
-  pinMode(ledPin, OUTPUT);
+  pinMode(blueLED, OUTPUT);
   pinMode(buttonApin, INPUT_PULLUP);  
   pinMode(buttonBpin, INPUT_PULLUP);  
 }
 
 void loop() 
 {
-  if (digitalRead(buttonApin) == LOW)
-  {
-    digitalWrite(ledPin, HIGH);
-      
-  }
-  else if (digitalRead(buttonApin) == HIGH)
-  {
-    digitalWrite(ledPin, LOW);
-  }
-  
-  if (digitalRead(buttonBpin) == LOW)
-  {
-    digitalWrite(greenLED, HIGH);
-  }
-  else if (digitalRead(buttonBpin) == HIGH)
-  {
-    digitalWrite(greenLED, LOW);
-  }
+
+  blueVal =digitalRead(buttonApin);
+  digitalWrite(blueLED, !blueVal);
+
+  greenVal = digitalRead(buttonBpin);
+  digitalWrite(greenLED, !greenVal);
+ 
 }
